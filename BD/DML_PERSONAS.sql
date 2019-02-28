4ee6343aeff8058ddfdd3aa96bdba4fc36f01dbd
JCortes87 <jcortesh1987@gmail.com>
2019-02-27 20:48:43 -0500

    Implementación de Login con validación de usuario

---
+++ BD/DML_PERSONAS.sql
@@ -0,0 +1,7 @@
+use carcenter
+
+INSERT INTO PERSONAS VALUES(
+
+'1234','Jose Cortés', 365478963, 'Calle falsa 123', 'jcortesh1987@gmail.com' , 
+ null, 'ADM' , '1234' , 'A' 
+);
--- CarcenterWeb/CarcenterWeb/Controllers/AccesoController.cs
+++ CarcenterWeb/CarcenterWeb/Controllers/AccesoController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarcenterWeb.Models;
 
 namespace CarcenterWeb.Controllers
 {
     public class AccesoController : Controller
     {
         // GET: Acceso
-        public ActionResult Index() 
+        public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Enter(int user, string pwd)
+        {
+            try
+            {
+                using (carcenterEntities db = new carcenterEntities())
+                {
+                    var lstPersonas = from p in db.PERSONAS
+                                      where p.IDENTIFICACION == user && p.CONTRASENA == pwd
+                                      select p;
+
+                    if (lstPersonas.Count() > 0)
+                    {
+                        PERSONAS oPersona = lstPersonas.First();
+                        Session["User"] = oPersona;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario o contraseña incorrectos");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Content("Error: " + ex.Message);
+            }
+
+        }
+
     }
 }
